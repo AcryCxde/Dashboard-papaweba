2bd9a0d4f89656ca2bce6868c8ac4a04463f30ec
AcryCxde <130891938+AcryCxde@users.noreply.github.com>
2025-01-11 17:18:45 +0500

    optimized upload_files

--- app/ETL/upload_files.py
+++ app/ETL/upload_files.py
@@ -1,58 +1,57 @@
 from extract import *
 from models.models import *
 
-top_headers_list = []
-side_headers_list = []
+# Кэш для объектов
+city_cache = {}
+year_cache = {}
+section_cache = {}
+side_headers_cache = {}
+top_headers_cache = {}  # Был список, заменен на словарь
+
+
+def get_or_create_cached(model, cache, **kwargs):
+    key = tuple(kwargs.items())
+    if key not in cache:
+        obj, created = model.get_or_create(**kwargs)
+        cache[key] = obj
+    return cache[key]
 
 
 def upload_files(files: list):
     for file in files:
-        # city, year = file.split()  # todo: после ручки от бека нужно брать file.name
-
+        # Определение города и года из имени файла
         file_city = ' '.join(file.split("\\")[-1].split()[:-1])
         file_year = int(file.split("\\")[-1].split()[1].split('.')[0])
 
         for section_number in range(1, 7):
             res = extract_section(file, section_number)
-
             res = create_section(res, file_city, file_year, section_number)
             print(res)
 
 
 def create_section(section, city, year, section_number):
-    global side_headers_list
-    global top_headers_list
+    # Получение объектов из кэша или базы данных
+    city_obj = get_or_create_cached(City, city_cache, city=city)
+    year_obj = get_or_create_cached(Year, year_cache, year=year)
+    section_obj = get_or_create_cached(Section, section_cache, section=section_number)
 
-    city_obj, is_city_created = City.get_or_create(city=city)
-    year_obj, is_year_created = Year.get_or_create(year=year)
-    section_obj, is_section_created = Section.get_or_create(section=section_number)
+    # Кэширование заголовков строк и столбцов
+    side_headers_list = [
+        get_or_create_cached(SideHeaders, side_headers_cache, header=name)
+        for name in section['side_headers']
+    ]
+    top_headers_list = [
+        get_or_create_cached(TopHeaders, top_headers_cache, header=name)
+        for name in section['top_headers']
+    ]
 
-    if not is_city_created:
-        city_obj.save()
-
-    if not is_year_created:
-        year_obj.save()
-
-    if not is_section_created:
-        section_obj.save()
-
-    if is_city_created or is_year_created or is_section_created:
-        for side_header_name in section['side_headers']:
-            side_header, is_side_created = SideHeaders.get_or_create(header=side_header_name)
-            if not is_side_created:
-                side_header.save()
-            side_headers_list.append(side_header)
-
-        for top_header_name in section['top_headers']:
-            top_header, is_top_created = TopHeaders.get_or_create(header=top_header_name)
-            if not is_top_created:
-                top_header.save()
-            top_headers_list.append(top_header)
-
-        for row_id, row in enumerate(section['data']):
-            for column_id in range(len(row)):
-                if value := row[column_id]:
-                    data = Data(
+    # Создание данных для пакетной вставки
+    data_objects = []
+    for row_id, row in enumerate(section['data']):
+        for column_id in range(len(row)):
+            if value := row[column_id]:
+                data_objects.append(
+                    Data(
                         top_header=top_headers_list[column_id],
                         side_header=side_headers_list[row_id],
                         city=city_obj,
@@ -60,7 +59,10 @@ def create_section(section, city, year, section_number):
                         section=section_obj,
                         value=value
                     )
-                    data.save()
+                )
+
+    # Пакетная вставка в базу данных
+    Data.bulk_create(data_objects)
 
     return f'Section {section_number} is Done'
 
