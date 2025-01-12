79894f856ab176fee12e81d758a9f3239ed36e98
AcryCxde <130891938+AcryCxde@users.noreply.github.com>
2025-01-12 23:16:01 +0500

    updated pages interface

--- app/frontend/src/pages/RecentlyCreated.jsx
+++ app/frontend/src/pages/RecentlyCreated.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
-import '../styles/App.css';
+import '../styles/RecentlyCreated.css';
 
-function RecentlyCreated() {
+const Table = () => {
+  const data = [
+    { name: "Текст", fields: "Текст", date: "11.01.2025" },
+    { name: "Текста тут Текст много", fields: "ОГРОООООМНЫЙ ТЕКСТ", date: "10.01.2025" },
+    { name: "Текст овоаыо оаыо ыыооа ыроа ооыа раыо ораоо рыыоа роы", fields: "Текст", date: "10.01.2025" },
+    { name: "Текст", fields: "Текстттттттттттттттттттттттттттттттттттттттттттттттт", date: "10.01.2025" },
+  ];
+
   return (
     <div className="page-container">
-      <h1>Недавно созданные</h1>
+      <h1 className="recent">Недавние</h1>
       <div className="page-content">
-        <p>Здесь вы найдете недавно созданные записи.</p>
       </div>
+        <table className="custom-table">
+          <thead>
+            <tr>
+              <th>Название</th>
+              <th>Поля</th>
+              <th>Дата создания</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.fields}</td>
+                <td>{row.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
     </div>
   );
-}
+};
 
-export default RecentlyCreated;
+export default Table;
--- app/frontend/src/pages/UploadTables.jsx
+++ app/frontend/src/pages/UploadTables.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
-import '../styles/App.css';
+import '../styles/UploadTables.css';
 
-function UploadTables() {
+const Table = () => {
+  const data = [
+    { name: "Волейбол 2020-2022", fields: "Всего, Волейбол", date: "11.01.2025" },
+    { name: "Новые работники", fields: "Специалисты, впервые принявшие ислам, маленькая строка получилась :(", date: "10.01.2025" },
+    { name: "Новые работники", fields: "Специалисты, впервые при...", date: "10.01.2025" },
+    { name: "Новые работники", fields: "Специалисты, впервые при...", date: "10.01.2025" },
+  ];
+
   return (
     <div className="page-container">
-      <h1>Загрузить таблицы</h1>
+      <button className="upload-table-button">Загрузить таблицы</button>
       <div className="page-content">
-        <p>Выберите и загрузите таблицы в систему.</p>
       </div>
+        <table className="custom-table">
+          <thead>
+            <tr>
+              <th>Имя пользователя</th>
+              <th>Количество таблиц</th>
+              <th>Дата создания</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.fields}</td>
+                <td>{row.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
     </div>
   );
-}
+};
 
-export default UploadTables;
+export default Table;
